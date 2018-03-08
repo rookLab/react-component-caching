9e104f32b31e6f9fc78aedb5ca25d66f95dbe0c8
Steven Lee <info@steven-lee.com>
2018-03-07 16:15:52 -0800

    adding mvp demo

---
+++ mvpDemo/benchmarkedFns.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOMServer from "./ModifiedReact";
+// import ReactDOMServer from "react-dom/server";
+import RecursiveDivs from "./RecursiveDivs";
+
+const cache = {};
+
+const depth = 4,
+  breadth = 11;
+
+const render = () => {
+  ReactDOMServer.renderToString(
+    // <RecursiveDivs depth={depth} breadth={breadth} cacheKey="test"/>,
+    <RecursiveDivs depth={depth} breadth={breadth} />,
+    cache
+  );
+};
+
+export default [{ name: "Testing render time", fn: render }];
