fb1d072b7d82d3b45a115fbaf56f47e266bac675
Annie <annie.difiore@gmail.com>
2018-03-10 15:32:05 -0800

    remove fake package

--- npmPackage/index.js
+++ npmPackage/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 if (process.env.NODE_ENV === 'production') {
+  // NOTE: CHANGE TO PRODUCTION WHEN WE HAVE THAT FINAL FILE
   module.exports = require('./development.js');
 } else {
   module.exports = require('./development.js');
