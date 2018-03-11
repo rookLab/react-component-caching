024a4123946292099d403117dd35f1eef59371ad
Mejin Leechor <amleechor@gmail.com>
2018-03-10 17:10:16 -0800

    Add product info component

--- SSRtest/src/shared/List.js
+++ SSRtest/src/shared/List.js
@@ -8,27 +8,17 @@ import './app.styl';
  */
 export default class List extends Component {
     render() {
-<<<<<<< HEAD
-        const listArr = [];
-        for (let i = 0; i < 10000; i++) {
-            listArr.push(<ListItem />)
-=======
-        let bunchOfPics = [];
-        for (let i=0; i<1000; i++) {
-            bunchOfPics.push(<ListItem key={i}/>);
->>>>>>> ff1de9f113946f852ded727878248e3f6fe66fe8
+        let bunchOfProducts = [];
+        for (let i=0; i<100; i++) {
+            bunchOfProducts.push(<Product key={i} name={`Thing${i}`} description={'This product is awesome!'} price={Math.floor(Math.random() * 200)} />);
         }
         return (
             <div>
               <h1>Here's my list</h1>
-<<<<<<< HEAD
-              {listArr}
-=======
-              {bunchOfPics}
+              {bunchOfProducts}
               {/* <ListItem />
               <ListItem />
               <ListItem /> */}
->>>>>>> ff1de9f113946f852ded727878248e3f6fe66fe8
             </div>
         );
     }
