92ac2c78d32e3ac617ba8169cf88fcb8522e3120
Annie <annie.difiore@gmail.com>
2018-04-08 19:12:13 -0700

    modular source files

--- src/server/ReactDOMStringRenderer.js
+++ src/server/ReactDOMStringRenderer.js
@@ -1,13 +1,20 @@
-var ReactDOMServerRenderer = require('./index').ReactDOMServerRenderer;
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import ReactPartialRenderer from './ReactPartialRenderer';
 
 /**
  * Render a ReactElement to its initial HTML. This should only be used on the
  * server.
  * See https://reactjs.org/docs/react-dom-server.html#rendertostring
  */
-async function renderToString(element, cache, memLife = 0) {
+export async function renderToString(element, cache, memLife=0) {
   // If and only if using memcached, pass the lifetime of your cache entry (in seconds) into 'memLife'.
-  var renderer = new ReactDOMServerRenderer(element, false);
+  var renderer = new ReactPartialRenderer(element, false);
   var markup = await renderer.read(Infinity, cache, false, null, memLife);
   return markup;
 }
@@ -17,13 +24,8 @@ async function renderToString(element, cache, memLife = 0) {
  * such as data-react-id that React uses internally.
  * See https://reactjs.org/docs/react-dom-server.html#rendertostaticmarkup
  */
-async function renderToStaticMarkup(element, cache, memLife = 0) {
-  var renderer = new ReactDOMServerRenderer(element, true);
+export async function renderToStaticMarkup(element, cache, memLife=0) {
+  var renderer = new ReactPartialRenderer(element, true);
   var markup = await renderer.read(Infinity, cache, false, null, memLife);
   return markup;
-}
-
-export default {
-  renderToString,
-  renderToStaticMarkup
 }
